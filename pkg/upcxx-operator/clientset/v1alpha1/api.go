5c20b71294ce7ecc2b37d6e3ad14cc5e4422a410
lnikon <bejanyan.vahag@protonmail.com>
2021-10-31 23:25:42 +0400

    Add clientset creation

---
+++ pkg/upcxx-operator/clientset/v1alpha1/api.go
@@ -0,0 +1,38 @@
+package v1alpha1
+
+import (
+	"github.com/lnikon/glfs-pkg/pkg/upcxx-operator/api/v1alpha1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+)
+
+type ExampleV1Alpha1Interface interface {
+	Projects(namespace string) UPCXXInterface
+}
+
+type ExampleV1Alpha1Client struct {
+	restClient rest.Interface
+}
+
+func NewForConfig(c *rest.Config) (*ExampleV1Alpha1Client, error) {
+	config := *c
+	config.ContentConfig.GroupVersion = &schema.GroupVersion{Group: v1alpha1.GroupVersion.Group, Version: v1alpha1.GroupVersion.Version}
+	config.APIPath = "/apis"
+	config.NegotiatedSerializer = scheme.Codecs.WithoutConversion()
+	config.UserAgent = rest.DefaultKubernetesUserAgent()
+
+	client, err := rest.RESTClientFor(&config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ExampleV1Alpha1Client{restClient: client}, nil
+}
+
+func (c *ExampleV1Alpha1Client) Projects(namespace string) UPCXXInterface {
+	return &UPCXXClient{
+		restClient: c.restClient,
+		ns:         namespace,
+	}
+}
